69ca7060db6af77b207ef971fdf72880ffac9e41
AZ366098 <84701052+Andy366098@users.noreply.github.com>
2021-12-21 14:05:10 +0800

    把碰撞偵測加進去 但沒使用

--- Assets/SteeringBehavior.cs
+++ Assets/SteeringBehavior.cs
@@ -115,7 +115,62 @@ public class SteeringBehavior
         }
         
     }
-    static public bool CollisionAvoid(AIData data)
+    static bool CheckCollision(AIData data)
+    {
+        //利用Main管理器獲得障礙物資訊
+        List<Obstacle> m_AvoidTargets = Main.m_Instance.GetObstacles();
+        if (m_AvoidTargets == null)
+        {
+            return false;
+        }
+        //先將常要操作的物件拿出來
+        Transform ct = data.m_Go.transform;
+        Vector3 cPos = ct.position;
+        Vector3 cForward = ct.forward;
+        data.m_vCurrentVector = cForward;
+        //
+        Vector3 vec;
+        Vector3 vFinalVec = Vector3.forward;
+        float fDist = 0.0f;
+        float fDot = 0.0f;
+        int iCount = m_AvoidTargets.Count;
+        for (int i = 0; i < iCount; i++)
+        {
+            //獲得往迴避目標的向量
+            vec = m_AvoidTargets[i].transform.position - cPos;
+            vec.y = 0.0f;   //不考慮Y變量
+            fDist = vec.magnitude;  //獲得距離
+            if (fDist > data.m_fProbeLength + m_AvoidTargets[i].m_fRadius) //距離如果超出探針跟物體半徑
+            {
+                //將該障礙物的狀態設為Outside，只影響其Gizimo的顏色
+                m_AvoidTargets[i].m_eState = Obstacle.eState.OUTSIDE_TEST;
+                continue;
+            }
+
+            vec.Normalize();    //正規化
+            fDot = Vector3.Dot(vec, cForward); //取得往目標向量在目前面相的投影
+            if (fDot < 0)   //內積小於0表示目標在身後
+            {
+                m_AvoidTargets[i].m_eState = Obstacle.eState.OUTSIDE_TEST;
+                continue;
+            }
+            else if (fDot > 1.0f)  //若因誤差超出1.0f
+            {
+                fDot = 1.0f;
+            }
+            m_AvoidTargets[i].m_eState = Obstacle.eState.INSIDE_TEST;
+            float fProjDist = fDist * fDot; //把它長度乘回去
+            //目標向量跟forward向量夾角的那個邊，也就是斜邊平方減一邊的平方
+            float fDotDist = Mathf.Sqrt(fDist * fDist - fProjDist * fProjDist);
+            if (fDotDist > m_AvoidTargets[i].m_fRadius + data.m_fRadius)    //如果沒有撞到
+            {
+                continue;
+            }
+            return true;    //上述條件都沒過表示會撞到
+        }
+        return false;
+    }
+    static bool CollisionAvoid(AIData data)
     {
         //利用Main管理器獲得障礙物資訊
         List<Obstacle> m_AvoidTargets = Main.m_Instance.GetObstacles();
@@ -123,6 +178,80 @@ public class SteeringBehavior
         Transform ct = data.m_Go.transform;
         Vector3 cPos = ct.position;
         Vector3 cForward = ct.forward;
-        return true;
+        data.m_vCurrentVector = cForward;
+        //
+        Vector3 vec;
+        Vector3 vFinalVec = Vector3.forward;
+        Obstacle oFinal = null;
+        float fDist = 0.0f;
+        float fDot = 0.0f;
+        float fFinalDot = 0.0f;
+        int iCount = m_AvoidTargets.Count;
+
+        float fMinDist = 10000.0f;
+        for (int i = 0; i < iCount; i++)
+        {
+            //獲得往迴避目標的向量
+            vec = m_AvoidTargets[i].transform.position - cPos;
+            vec.y = 0.0f;   //不考慮Y變量
+            fDist = vec.magnitude;  //獲得距離
+            if (fDist > data.m_fProbeLength + m_AvoidTargets[i].m_fRadius) //距離如果超出探針跟物體半徑
+            {
+                //將該障礙物的狀態設為Outside，只影響其Gizimo的顏色
+                m_AvoidTargets[i].m_eState = Obstacle.eState.OUTSIDE_TEST;  
+                continue;
+            }
+
+            vec.Normalize();    //正規化
+            fDot = Vector3.Dot(vec, cForward); //取得往目標向量在目前面相的投影
+            if (fDot < 0)   //內積小於0表示目標在身後
+            {
+                m_AvoidTargets[i].m_eState = Obstacle.eState.OUTSIDE_TEST;
+                continue;
+            }else if (fDot > 1.0f)  //若因誤差超出1.0f
+            {
+                fDot = 1.0f;
+            }
+            m_AvoidTargets[i].m_eState = Obstacle.eState.INSIDE_TEST;
+            float fProjDist = fDist * fDot; //把它長度乘回去
+            //目標向量跟forward向量夾角的那個邊，也就是斜邊平方減一邊的平方
+            float fDotDist = Mathf.Sqrt(fDist * fDist - fProjDist * fProjDist);
+            if (fDotDist > m_AvoidTargets[i].m_fRadius + data.m_fRadius)    //如果沒有撞到
+            {
+                continue;
+            }
+            if (fDist < fMinDist)   //將最近的障礙物資訊存起來
+            {
+                fMinDist = fDist;
+                vFinalVec = vec;
+                oFinal = m_AvoidTargets[i];
+                fFinalDot = fDot;
+            }
+        }
+        if (oFinal != null) //防呆，避免忘了加障礙物閃退
+        {
+            Vector3 vCross = Vector3.Cross(cForward, vFinalVec);//換一個方式，用外積來算要左轉還右轉
+            float fTurnMag = Mathf.Sqrt(1.0f - fFinalDot * fFinalDot);  //因為finalDot已經單位化，因此用1-FinalDot也可得知對邊的長度
+            if (vCross.y > 0.0f)    //如果是正的代表要向左轉
+            {
+                fTurnMag = -fTurnMag;
+            }
+            data.m_fTempTurnForce = fTurnMag;
+
+            float fTotalLen = data.m_fProbeLength + oFinal.m_fRadius;
+            float fRatio = fMinDist / fTotalLen;//力的比率，探針跟物體半徑長的和分之距離
+            if (fRatio > 1.0f)   //防呆，比率為0~1的數值
+            {
+                fRatio = 1.0f;
+            }
+            fRatio = 1.0f - fRatio; //越靠近力的比率要越大
+            data.m_fMoveForce = -fRatio;    //永遠為斥力
+            oFinal.m_eState = Obstacle.eState.COL_TEST; //變物體的狀態以讓他變顏色
+            data.m_bCol = true;
+            data.m_bMove = true;
+            return true;
+        }
+        data.m_bCol = false;
+        return false;
     }
 }
